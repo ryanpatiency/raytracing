ab1f711780292d692a89b18954cf95ed4d6e0a59
ryanpatiency <ryanpatiency@gmail.com>
2017-03-01 22:36:43 +0800

    Use one line implement dot_product

--- math-toolkit.h
+++ math-toolkit.h
@@ -61,7 +61,7 @@ void cross_product(const double *v1, const double *v2, double *out)
 static inline
 double dot_product(const double *v1, const double *v2)
 {
-    return v1[0]*v2[0]+v1[1]*v2[1]+v1[2]*v2[2];
+    return v1[0]*v1[0] + v1[1]*v2[1] + v1[2]*v2[2];
 }
 
 static inline
